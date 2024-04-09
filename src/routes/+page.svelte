ecabde7c72551c3e342141b48049bbf06b2ccd82
Neville Brem <bremneville@gmail.com>
2024-04-09 10:32:43 +0200

    tweaked some shit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,19 +7,19 @@
 </script>
 
 <main class="max-w-[70vw] mx-auto flex flex-col justify-between h-screen p-16 gap-4">
-	<h1 class="h1 mb-4">Your chat:</h1>
+	<h1 class="h1 mb-4 select-none">Your chat:</h1>
 	<div class="overflow-auto">
 		<ul class="flex flex-col gap-8">
 			{#each $messages as msg}
 				<li class="flex flex-col">
 					<p class="font-medium mb-2">{msg.role} on {msg.createdAt?.toDateString()}:</p>
-					<p class="prose text-white">{@html marked(msg.content)}</p>
+					<p class="prose prose-code:text-white text-white">{@html marked(msg.content)}</p>
 				</li>
 			{/each}
 		</ul>
 	</div>
 	<form on:submit={handleSubmit} class="flex gap-4">
-		<input class="input p-2" bind:value={$input} />
+		<input placeholder="Your query" class="input p-2" bind:value={$input} />
 		<button class="btn variant-ghost-primary" type="submit">Send</button>
 	</form>
 </main>
