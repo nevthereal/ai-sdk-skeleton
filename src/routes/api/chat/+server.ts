e8b64f8782ab74027aef5cadd7772f505e0c38e4
Neville Brem <bremneville@gmail.com>
2024-05-16 20:18:15 +0200

    implemented the tool, but not working yet

--- src/routes/api/chat/+server.ts
+++ src/routes/api/chat/+server.ts
@@ -1,12 +1,13 @@
 import { createOpenAI } from '@ai-sdk/openai';
 import { StreamingTextResponse, streamText, tool } from 'ai';
+import wretch from 'wretch';
 
-import { env } from '$env/dynamic/private';
+import { OPENAI_KEY, WEATHER_KEY } from '$env/static/private';
 import type { RequestHandler } from './$types';
 import { z } from 'zod';
 
 const openai = createOpenAI({
-	apiKey: env.OPENAI_KEY ?? ''
+	apiKey: OPENAI_KEY ?? ''
 });
 
 export const POST = (async ({ request }) => {
@@ -16,14 +17,28 @@ export const POST = (async ({ request }) => {
 		model: openai('gpt-4-turbo-preview'),
 		tools: {
 			weather: tool({
-				description: 'Get the weather in a location',
+				description: 'Get the temperature of a location in Fahrenheit',
 				parameters: z.object({
-					location: z.string().describe('The location to get the weather for')
+					location: z.string().describe('The location to get the temperature from')
 				}),
-				execute: async ({ location }) => ({
-					location,
-					temperature: 72 + Math.floor(Math.random() * 21) - 10
-				})
+				execute: async ({ location }) => {
+					const locData = await wretch(
+						`http://api.openweathermap.org/geo/1.0/direct?q=${location}&appid=${WEATHER_KEY}`
+					)
+						.get()
+						.json((json) => {
+							return { lat: json[0].lat, lon: json[0].lon };
+						});
+
+					const temperature = await wretch(
+						`https://api.openweathermap.org/data/3.0/onecall?lat=${locData.lat}&lon=${locData.lon}&appid=${WEATHER_KEY}`
+					)
+						.get()
+						.json((json) => {
+							return json.current.temp;
+						});
+					return temperature;
+				}
 			})
 		},
 		messages
